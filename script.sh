e9fd6f07b4688560e41a8b3495204afa29f977ff
Alonso Kraemer <kraemer.2002@alunos.utfpr.edu.br>
2024-11-19 20:45:16 -0300

    Subindo os faltantes

---
+++ script.sh
@@ -0,0 +1,5 @@
+total 4
+-rw-r--r-- 1 devops devops  0 nov 19 20:39 lista.ls
+-rw-r--r-- 1 devops devops  0 nov 19 20:03 nova_branch.txt
+-rw-r--r-- 1 devops devops 16 nov 19 20:22 olagit.txt
+-rw-r--r-- 1 devops devops  0 nov 19 20:42 script.sh
